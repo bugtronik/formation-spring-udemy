e755052960ea6794ae74e1c82d4035bd3d6ccfbc
Aly <aly.mboumba@setrag.com>
2022-08-11 11:25:16 +0100

    Développement du contrôleur Borrow

---
+++ demo/src/main/java/com/udemy/demo/borrow/Borrow.java
@@ -0,0 +1,47 @@
+package com.udemy.demo.borrow;
+
+import java.time.LocalDate;
+
+import com.udemy.demo.book.Book;
+import com.udemy.demo.user.User;
+
+public class Borrow {
+	
+	private User borrower;
+	private User lender;
+	private Book book;
+	private LocalDate askDate;
+	private LocalDate closeDate;
+	
+	public LocalDate getAskDate() {
+		return askDate;
+	}
+	public void setAskDate(LocalDate askDate) {
+		this.askDate = askDate;
+	}
+	public LocalDate getCloseDate() {
+		return closeDate;
+	}
+	public void setCloseDate(LocalDate closeDate) {
+		this.closeDate = closeDate;
+	}
+	public User getBorrower() {
+		return borrower;
+	}
+	public void setBorrower(User borrower) {
+		this.borrower = borrower;
+	}
+	public User getLender() {
+		return lender;
+	}
+	public void setLender(User lender) {
+		this.lender = lender;
+	}
+	public Book getBook() {
+		return book;
+	}
+	public void setBook(Book book) {
+		this.book = book;
+	}
+
+}
---
+++ demo/src/main/java/com/udemy/demo/borrow/BorrowController.java
@@ -0,0 +1,20 @@
+package com.udemy.demo.borrow;
+
+import java.time.LocalDate;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class BorrowController {
+	
+	@GetMapping(value="/borrows")
+	public ResponseEntity getMyBorrows() {
+		Borrow borrow = new Borrow();
+		borrow.setAskDate(LocalDate.now());
+		return new ResponseEntity(borrow, HttpStatus.OK);
+	}
+
+}
