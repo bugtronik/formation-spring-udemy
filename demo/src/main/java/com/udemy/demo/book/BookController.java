c30443f9d58241942b289f8f07f2f973dcc6e06f
Aly <aly.mboumba@setrag.com>
2022-08-11 10:50:03 +0100

    Developpement du controleur Book

---
+++ demo/src/main/java/com/udemy/demo/book/BookController.java
@@ -0,0 +1,43 @@
+package com.udemy.demo.book;
+
+import java.util.Arrays;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class BookController {
+
+	@GetMapping(value="/books")
+	public ResponseEntity listBooks() {
+		Book book = new Book();
+		book.setTitle("Maths");
+		book.setCategory(new Category("BD"));
+		
+		return new ResponseEntity(Arrays.asList(book), HttpStatus.OK);
+	}
+	
+	@PostMapping(value="/books")
+	public ResponseEntity addBook(Book book) {
+		
+		return new ResponseEntity(book, HttpStatus.CREATED);
+	}
+	
+	@DeleteMapping(value="/books/{bookId}")
+	public ResponseEntity deleteBook(@PathVariable("bookId") String bookId) {
+		
+		return new ResponseEntity(HttpStatus.NO_CONTENT);
+	}
+	
+	@PutMapping(value="/books/{bookId}")
+	public ResponseEntity updateBook(@PathVariable("bookId") String bookId, Book book) {
+		
+		return new ResponseEntity(HttpStatus.OK);
+	}
+}
---
+++ demo/src/main/java/com/udemy/demo/book/Category.java
@@ -0,0 +1,19 @@
+package com.udemy.demo.book;
+
+public class Category {
+
+	private String label;
+
+	public String getLabel() {
+		return label;
+	}
+
+	public Category(String label) {
+		super();
+		this.label = label;
+	}
+
+	public void setLabel(String label) {
+		this.label = label;
+	}
+}
