e81d0e7600da43b177db9cee51237877e6e2e053
Aly <aly.mboumba@setrag.com>
2022-08-12 15:03:02 +0100

    Branchement au repository: mes livres et livres disponibles

--- demo/src/main/java/com/udemy/demo/book/BookController.java
+++ demo/src/main/java/com/udemy/demo/book/BookController.java
@@ -1,9 +1,11 @@
 package com.udemy.demo.book;
 
 import java.util.Arrays;
+import java.util.List;
 
 import javax.validation.Valid;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -12,20 +14,35 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class BookController {
+	
+	@Autowired
+	private BookRepository bookRepository;
 
 	@GetMapping(value="/books")
-	public ResponseEntity listBooks() {
-		Book book = new Book();
-		book.setTitle("Maths");
-		book.setCategory(new Category("BD"));
+	public ResponseEntity listBooks(@RequestParam(required = false) BookStatus status) {
 		
-		return new ResponseEntity(Arrays.asList(book), HttpStatus.OK);
+		Integer userConnectedId = this.getUserConnectId();
+		List<Book> books;
+		
+		if(status != null && status == BookStatus.FREE) {
+			books = bookRepository.findByStatusAndUserIdNotAndDeletedFalse(status, userConnectedId);
+		} else {
+			books = bookRepository.findByUserIdAndDeletedFalse(userConnectedId);
+		}
+		
+		return new ResponseEntity(books, HttpStatus.OK);
 	}
 	
+	private Integer getUserConnectId() {
+		// TODO Auto-generated method stub
+		return 1;
+	}
+
 	@PostMapping(value="/books")
 	public ResponseEntity addBook(@RequestBody @Valid Book book) {
 		
