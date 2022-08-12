91bb8fd4b4eca7e929a2ae6e3a2d411e9a5ff874
Aly <aly.mboumba@setrag.com>
2022-08-12 09:00:09 +0100

    Intégration de JPA et finition de l'entité Book

--- demo/src/main/java/com/udemy/demo/book/Book.java
+++ demo/src/main/java/com/udemy/demo/book/Book.java
@@ -1,14 +1,70 @@
 package com.udemy.demo.book;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.Transient;
 import javax.validation.constraints.NotBlank;
 
+import com.udemy.demo.user.User;
+
+
+@Entity
 public class Book {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
 
 	@NotBlank
 	private String title;
+	
+	@ManyToOne
 	private Category category;
 	
+	@ManyToOne
+	private User user;
 	
+	private BookStatus status;
+	
+	private boolean deleted;
+	
+	@Transient
+	private int categoryId;
+	
+	
+	public boolean isDeleted() {
+		return deleted;
+	}
+	public void setDeleted(boolean deleted) {
+		this.deleted = deleted;
+	}
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public User getUser() {
+		return user;
+	}
+	public void setUser(User user) {
+		this.user = user;
+	}
+	public BookStatus getStatus() {
+		return status;
+	}
+	public void setStatus(BookStatus status) {
+		this.status = status;
+	}
+	public int getCategoryId() {
+		return categoryId;
+	}
+	public void setCategoryId(int categoryId) {
+		this.categoryId = categoryId;
+	}
 	public String getTitle() {
 		return title;
 	}
