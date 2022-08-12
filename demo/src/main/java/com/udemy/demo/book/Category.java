a83fedb42e6c0002d2998d4a28f082c39adde797
Aly <aly.mboumba@setrag.com>
2022-08-12 09:18:52 +0100

    Dev de l'entité Category

--- demo/src/main/java/com/udemy/demo/book/Category.java
+++ demo/src/main/java/com/udemy/demo/book/Category.java
@@ -1,8 +1,26 @@
 package com.udemy.demo.book;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Category {
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	
 	private String label;
+	
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
 
 	public String getLabel() {
 		return label;
