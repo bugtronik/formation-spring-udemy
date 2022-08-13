a06f338830b0a7ab46669f020b4530f0562bf7e7
bugtronik <alynopunchline@gmail.com>
2022-08-14 00:08:05 +0100

    Ajout livre

--- demo/src/main/java/com/udemy/demo/book/BookController.java
+++ demo/src/main/java/com/udemy/demo/book/BookController.java
@@ -2,6 +2,7 @@ package com.udemy.demo.book;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
 
 import javax.validation.Valid;
 
@@ -17,11 +18,20 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.udemy.demo.user.User;
+import com.udemy.demo.user.UserRepository;
+
 @RestController
 public class BookController {
 	
 	@Autowired
 	private BookRepository bookRepository;
+	
+	@Autowired
+	private UserRepository userRepository;
+	
+	@Autowired
+	private CategoryRepository categoryRepository;
 
 	@GetMapping(value="/books")
 	public ResponseEntity listBooks(@RequestParam(required = false) BookStatus status) {
@@ -45,6 +55,23 @@ public class BookController {
 
 	@PostMapping(value="/books")
 	public ResponseEntity addBook(@RequestBody @Valid Book book) {
+		
+		Integer userConnectedId = this.getUserConnectId();
+		Optional<User> user = userRepository.findById(userConnectedId);
+		Optional<Category> category = categoryRepository.findById(book.getCategoryId());
+		if(category.isPresent()) {
+			book.setCategory(category.get());
+		} else {
+			return new ResponseEntity("You must provide a valid category", HttpStatus.BAD_REQUEST);
+		}
+		if(user.isPresent()) {
+			book.setUser(user.get());
+		} else {
+			return new ResponseEntity("You must provide a valide user", HttpStatus.BAD_REQUEST);
+		}
+		book.setDeleted(false);
+		book.setStatus(BookStatus.FREE);
+		bookRepository.save(book);
 		
 		return new ResponseEntity(book, HttpStatus.CREATED);
 	}
