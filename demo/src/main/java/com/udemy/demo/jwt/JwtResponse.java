964b6fcc1a99569890046cdac856d550e3f8010e
Aly <aly.mboumba@setrag.com>
2022-08-23 11:05:26 +0100

    Codons l'authentication JWT: JwtController

---
+++ demo/src/main/java/com/udemy/demo/jwt/JwtResponse.java
@@ -0,0 +1,20 @@
+package com.udemy.demo.jwt;
+
+public class JwtResponse {
+	
+	private String userName;
+	
+	public JwtResponse(String userName) {
+		this.userName = userName;
+	}
+
+	public String getUserName() {
+		return userName;
+	}
+
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+	
+	
+}
---
+++ demo/src/main/java/com/udemy/demo/jwt/JwtUtils.java
@@ -0,0 +1,11 @@
+package com.udemy.demo.jwt;
+
+import org.springframework.security.core.Authentication;
+
+public class JwtUtils {
+
+	public String generateToken(Authentication authentication) {
+		return "";
+	}
+
+}
